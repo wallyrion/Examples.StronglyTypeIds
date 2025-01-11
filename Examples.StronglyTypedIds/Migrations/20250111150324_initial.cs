ed5f11b43edabfeaee0c46c72572d1557ff7ee02
Oleksii KorniienKo <kornienko1296@gmail.com>
2025-01-11 16:14:26 +0100

    add pets and owners

---
+++ Examples.StronglyTypedIds/Migrations/20250111150324_initial.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Examples.StronglyTypedIds.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Owners",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owners", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pets_Owners_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Owners",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_OwnerId",
+                table: "Pets",
+                column: "OwnerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pets");
+
+            migrationBuilder.DropTable(
+                name: "Owners");
+        }
+    }
+}
--- Examples.StronglyTypedIds/Program.cs
+++ Examples.StronglyTypedIds/Program.cs
@@ -1,3 +1,5 @@
+using Bogus;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,37 +7,92 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AppDbContext>(s =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("Default");
+    s.UseNpgsql(connectionString);
+});
 
 var app = builder.Build();
+
+await using (var scope = app.Services.CreateAsyncScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await dbContext.Database.MigrateAsync();
+}
+
 app.MapScalarApiReference();
 
 app.MapOpenApi();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+
+app.MapGet("/owners", async (AppDbContext dbContext) =>
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-
-        return forecast;
+        return await dbContext.Owners.ToListAsync();
     })
-    .WithName("GetWeatherForecast");
+    .WithName("Get all owners");
+
+app.MapPost("/owners", async (AppDbContext dbContext) =>
+    {
+        var owner = Fakers.Owner.Generate();
+        await dbContext.Owners.AddAsync(owner);
+        await dbContext.SaveChangesAsync();
+        return owner;
+    })
+    .WithName("Create random owner");
+
+
+app.MapPost("/owners/{ownerId:guid}/pet", async (AppDbContext dbContext, Guid ownerId) =>
+    {
+        var pet = Fakers.Pet(ownerId).Generate();
+        await dbContext.Pets.AddAsync(pet);
+        await dbContext.SaveChangesAsync();
+        return pet;
+    })
+    .WithName("Create random pet for owner");
+
+
+app.MapGet("/owners/{ownerId:guid}/pets", async (AppDbContext dbContext, Guid ownerId) =>
+    {
+        return await dbContext.Pets.Where(p => p.OwnerId == ownerId).ToListAsync();
+    })
+    .WithName("Get Pets by owner id");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public DbSet<Owner> Owners { get; set; }
+    public DbSet<Pet> Pets { get; set; }
+}
+
+public class Owner
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public List<Pet> Pets { get; set; } = [];
+}
+
+public class Pet
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public Guid OwnerId { get; set; }
+    public Owner Owner { get; set; } = null!;
+}
+
+public class Fakers
+{
+    public static Faker<Owner> Owner = new Faker<Owner>()
+        .RuleFor(o => o.Id, f => f.Random.Guid())
+        .RuleFor(o => o.Name, f => f.Name.FullName());
+    
+    
+    public static Faker<Pet> Pet (Guid ownerId) => new Faker<Pet>()
+        .RuleFor(p => p.Id, f => f.Random.Guid())
+        .RuleFor(p => p.Name, f => f.Name.FullName())
+        .RuleFor(p => p.OwnerId, f => ownerId);
 }
